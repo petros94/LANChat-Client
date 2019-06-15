693f05ce9ad6feda9e5d2c5a3be610f456348110
Petros Mitseas <petros_94@icloud.com>
2019-06-15 10:45:31 +0300

    fixed settings json path

--- src/app.java
+++ src/app.java
@@ -4,16 +4,25 @@ public class app {
 	static String OS = "";
 	
 	public static void main(String[] args) {
+		
+		String path = "";
+		if (args.length>0) {
+			path = args[0];
+		}
+		else {
+			path = "config/LANChat_settings_client.json";
+		}
+		
 		try {
 			OS = System.getProperty("os.name");
 			if (isWindows()) {
 				System.setProperty("java.library.path", args[1]);
 		    	System.load(args[1] +"/swt-win32-3139.dll");
 		    	System.out.println(System.getProperty("java.library.path"));
-				new GepChat_ChatWindow(args[0]);
+				new GepChat_ChatWindow(path);
 			}
 			else {
-				new GepChat_ChatWindow(args[0]);
+				new GepChat_ChatWindow(path);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
